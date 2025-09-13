d23c2bfbc97999c11623481f2def23a264354073
Renan <renanestery1@gmail.com>
2025-09-13 09:27:39 -0300

    feat: create module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './shared/prisma/prisma.module';
-
+import { PrismaModule } from '@shared/prisma/prisma.module';
+import { UsersModule } from '@modules/users/users.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
 })
 export class AppModule {}
